6dd8862bf56d59e879235ec8e05a13c1fc7ed539
EWanjiru <eunice.wanjiru@andela.com>
2018-04-27 12:18:23 +0300

    exercise2: make the REST routes to perform CRUD on lions

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 // TODO: make this work.
 // if yuo go to localhost:3000 the app
 // there is expected crud to be working here
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const _ = require('lodash');
 
 // express.static will serve everything
 // with in client as a static resource
@@ -18,10 +18,41 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-var lions = [];
-var id = 0;
+let lions = [{name:"Simba", id: "1", age:"3", pride:"King", gender:"male"}];
+let id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+
+app.get('/lions',(req, res)=>{
+    res.json(lions);
+});
+
+app.get('/lions/:id',(req, res)=>{
+    const lion = lions.filter(lion => lion.id===req.params.id);
+    res.json(lion);
+});
+
+app.post('/lions',(req, res)=>{
+    let data = req.body;
+    (data && data)? id++ : id;
+    data = Object.assign(data, {id: ''+id+''})
+    lions = [...lions, data];
+    res.json(lions);
+});
+
+app.put('/lions/:id',(req, res)=>{
+    const lionId = req.params.id;
+    const body = req.body;
+    let lion = lions.filter((lion, index) => lion.id===lionId);
+    lions.splice(index,1,body); 
+    res.json(lions);
+});
+
+app.delete('/lions/:id',(req, res)=>{
+    const lionId = req.params.id;
+    lions = lions.findIndex(lion => lion.id === lionId).splice(lion,1);
+    res.json(lions);
+});
 
 app.listen(3000);
 console.log('on port 3000');
